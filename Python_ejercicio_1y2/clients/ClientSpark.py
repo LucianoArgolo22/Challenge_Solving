6a0d60ba77a2cdc654ab0b8d97be61d1c3174a12
Luciano Agustín Argolo <75091406+LucianoArgolo22@users.noreply.github.com>
2023-02-14 10:30:56 -0300

    optimizing process
    - Optimized process for obtainig max malt, not using joins anymore.

--- Python_ejercicio_1y2/clients/ClientSpark.py
+++ Python_ejercicio_1y2/clients/ClientSpark.py
@@ -21,26 +21,31 @@ class ClientSpark:
                                  .when(df[column_name] == 'NaN'  ,"not found")
                                  .when(df[column_name].isNull()  ,"not found")
                                  .when(df[column_name] > numeric_values[1], string_values[2]) )
-
+    
     def whole_process(self, df:pd.DataFrame, initial_columns:list, final_columns:list) -> object:
         self.log.info(f' - ClientSpark - whole_process - Running whole processing ')
         df = self.spark.createDataFrame(df)
         df = df.select(initial_columns)
         df.createOrReplaceTempView('df')
-        df_max = self.spark.sql('''SELECT id, a.malts["name"] as principal_malt,
-                        cast(substring(a.malts["amount"], 8,3) as float) as kilograms 
-        from (
-            select id, Explode(ingredients["malt"]) as malts from df
-             ) a
+        df = self.spark.sql('''SELECT *, a.malts["name"] as principal_malt,
+                                    cast(substring(a.malts["amount"], 8,3) as float) as kilograms 
+                        from (
+                                select *, Explode(ingredients["malt"]) as malts from df
+                             ) a
         ''')
-        self.log.info(f' - ClientSpark - whole_process - Grouping max values for principal_malt ')
-        df2 = df_max.select('id', 'kilograms')
-        df2 = df2.groupBy("id").max("kilograms").withColumnRenamed('max(kilograms)', 'kilograms')
-        df2 = df_max.join(df2, ['id','kilograms'], 'inner').select('id','principal_malt').orderBy('id')
-        self.log.info(f' - ClientSpark - whole_process - Joined with max values for principal_malt ')
-        
+        self.log.info(f' - ClientSpark - whole_process - obtaining max values for principal_malt ')
+        df = df.select(['id', 'name', 'first_brewed', 'abv', 'ibu', 'ph','principal_malt', 'kilograms'])
+        df.createOrReplaceTempView('df2')
+        df = self.spark.sql('''select * 
+            from (
+                    select 
+                    *,
+                    row_number() over (partition by id order by id, kilograms desc) as max_malt
+                    from df2 
+                ) a
+            where max_malt = 1
+            ''')
         df = self.case_values(df=df, new_column_name='ph_type', column_name='ph', numeric_values=[7,7], string_values=['base', 'neutral', 'sour'])
         df = self.case_values(df=df, new_column_name='alcohol_type', column_name='abv', numeric_values=[5,7], string_values=['low', 'medium', 'strong'])
-        self.log.info(f' - ClientSpark - whole_process - Performing Join ')
-        df = df.join(df2, 'id', 'inner')
+        self.log.info(f' - ClientSpark - whole_process - Returning df ')
         return  df.select(final_columns)
