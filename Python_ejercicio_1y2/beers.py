023ee156093cd32e708875bae56d3736ed619484
Luciano Agustín Argolo <75091406+LucianoArgolo22@users.noreply.github.com>
2023-02-15 10:45:59 -0300

    Update beers.py

--- Python_ejercicio_1y2/beers.py
+++ Python_ejercicio_1y2/beers.py
@@ -20,16 +20,15 @@ if __name__=='__main__':
     initial_columns = ['id', 'name', 'first_brewed', 'abv', 'ibu', 'ph', 'ingredients']
     final_columns = ['id', 'name', 'first_brewed', 'abv', 'ibu', 'ph', 'principal_malt', 'ph_type', 'alcohol_type']
     
-    df_p = pd.DataFrame(response.json())
     spk = ClientSpark(log=log)
-    df = spk.whole_process(df=df_p, initial_columns=initial_columns, final_columns=final_columns)
-    df_p = df.toPandas()
+    df = spark.read.option("multiline", "true") \
+      .json(response.json())
+    df = spk.whole_process(df=df, initial_columns=initial_columns, final_columns=final_columns)
     if database:
         log.info('Using local Database')
-        psql = ClientPSQL(log=log, table='beers', df=df_p )
+        psql = ClientPSQL(log=log, table='beers', df=df )
         psql.inserting_rows()
     else:
         log.info('Using S3')
-        json_df = df_p.to_json()
         clients3 = ClientS3(log=log, file_name='parsed.json', python='python2')
-        clients3.upload_objects_to_s3(data=json_df)
+        clients3.upload_objects_to_s3(data=df)
